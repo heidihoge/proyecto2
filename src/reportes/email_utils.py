7fbfa49d8ade86589e6452da8ee39c28feb60d97
heidihoge <heidihoge93@gmail.com>
2018-07-10 14:29:27 -0400

    - Agrega funciones p/ envio de balance por correo

--- src/reportes/email_utils.py
+++ src/reportes/email_utils.py
@@ -1,18 +1,16 @@
 import csv
 import datetime
+from io import StringIO
 
 from django.http import HttpResponse
 
-from tienda.models import VentaCabecera
+from tienda.models import VentaCabecera, CompraCabecera, OperacionCaja
+
 
 def export_venta(resultados, fecha, fecha_fin=None, nombre='ventas-diarias'):
-    response = HttpResponse(content_type='text/csv')
-    fecha_1 = '{0:02}-{1:02}-{2}'.format(fecha.day, fecha.month, fecha.year)
-    fecha_2 = '-{0:02}-{1:02}-{2}'.format(fecha_fin.day, fecha_fin.month, fecha_fin.year) if fecha_fin else ''
-    response['Content-Disposition'] = 'attachment; filename="{0}-{1}{2}.csv"'\
-        .format(nombre, fecha_1, fecha_2)
+    csvfile = StringIO()
 
-    writer = csv.writer(response)
+    writer = csv.writer(csvfile)
     writer.writerow(['Fecha', 'Nro Factura', 'Tipo de pago', 'Ruc Cliente', 'Cliente', 'Gravada 10%', 'IVA 10%',
                      'Gravada 5%', 'IVA 5%', 'Exentas', 'Total IVA', 'Monto Total', 'Estado'])
 
@@ -33,18 +31,54 @@ def export_venta(resultados, fecha, fecha_fin=None, nombre='ventas-diarias'):
                          resultado.total_iva_5, resultado.total_grav_exentas, resultado.total_iva,
                          resultado.monto_total, texto_estado(resultado.estado)])
 
-    return response
+    return csvfile
 
 
 def ventas_diaras():
     ventas = VentaCabecera.objects.filter(fecha=datetime.date.today(), estado__in=['A'], tipo_pago='Contado')
     resp = export_venta(ventas, datetime.date.today())
-    return str(resp.content)
+    return resp.getvalue()
+
+
+
+def export_compra(resultados, fecha, fecha_fin=None, nombre='compras-diarias'):
+    csvfile = StringIO()
+
+    writer = csv.writer(csvfile)
+    writer.writerow(['Fecha', 'Nro Factura', 'Descripcion', 'Ruc Proveedor', 'Nombre Proveedor', 'Gravada 10%', 'IVA 10%',
+                    'Gravada 5%', 'IVA 5%', 'Exentas', 'Total IVA', 'Monto Total'])
+
+
+
+    for resultado in resultados:
+        writer.writerow([resultado.fecha, str(resultado.nro_factura), resultado.tipo_pago,
+                         resultado.ruc_proveedor,
+                         resultado.proveedor, resultado.total_grav_10,
+                         resultado.total_iva_10, resultado.total_grav_5,
+                         resultado.total_iva_5, resultado.total_grav_exentas, resultado.total_iva,
+                         resultado.monto_total])
+
+    return csvfile
 
 
 def compras_diaras():
-    return ""
+    compras = CompraCabecera.objects.filter(fecha=datetime.date.today())
+    resp = export_compra(compras, datetime.date.today())
+    return resp.getvalue()
 
+
+def export_operaciones(operaciones, fecha):
+    csvfile = StringIO()
+    writer = csv.writer(csvfile)
+    writer.writerow(['Fecha', 'Tipo transaccion', 'Monto', 'Concepto', 'Descripcion'])
+
+    for operacion in operaciones:
+        writer.writerow([operacion.fecha, operacion.tipo_transaccion, operacion.monto,
+                         operacion.concepto, operacion.descripcion])
+
+    return csvfile
 
 def caja_chica_diaras():
-    return ""
+    operaciones = OperacionCaja.objects.filter(fecha=datetime.date.today())
+    resp = export_operaciones(operaciones, datetime.date.today())
+    return resp.getvalue()
